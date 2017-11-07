9954a273af41f2123726772262aef1f0b5d74ad8
jovanna <ladybutterfly.fly@gmail.com>
2017-11-07 11:59:14 +0100

    postavljanje app-routing modula + router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
@@ -18,6 +19,7 @@ import { RegisterComponent } from './components/auth/register/register.component
   imports: [
     BrowserModule,
     SharedModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
